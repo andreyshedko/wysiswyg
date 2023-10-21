db33dac06ca01aff3d6e7cbf508987cc7d238f8e
andrey.shedko@gmail.com <andrey.shedko@gmail.com>
2023-10-21 21:32:37 +0200

    Basic setup and forn awesome

---
+++ src/lib/components/Editor.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Footer from './Footer.svelte';
+    import Icons from './Icons.svelte';
+    import Menu from './Menu.svelte';
+    import "../app.scss";
+</script>
+
+<Menu />
+<Icons />
+<textarea>
+</textarea>
+<Footer />
