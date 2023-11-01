2bb68821672d2845f3b93ff6c79ac4539bf64a5c
andrey.shedko@gmail.com <andrey.shedko@gmail.com>
2023-11-01 18:56:29 +0100

    setAppearance

--- src/lib/stores/text-component.store.ts
+++ src/lib/stores/text-component.store.ts
@@ -3,6 +3,6 @@ import { writable, type Writable } from "svelte/store";
 const text: Writable<Editor.TextElementProps> = writable({} as Editor.TextElementProps);
 export const content: Writable<Editor.TextElementProps> = text;
 
-export const setAppearance =  (type: "header" | "content"): void => {
-    content.update((state: Editor.TextElementProps) => ({...state, {appearance: {type}}));
+export const setAppearance = (type: "header" | "content"): void => {
+    content.update((state: Editor.TextElementProps) => ({ ...state, appearance: { ...state.appearance, type} }));
 }
