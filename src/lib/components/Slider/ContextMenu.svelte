12b0d0a8956854cb6d5dde9be5f6437b49c4c850
andrey.shedko@gmail.com <andrey.shedko@gmail.com>
2023-11-05 16:30:15 +0100

    Change icons to css, chnage to bulma css

---
+++ src/lib/components/Slider/ContextMenu.svelte
@@ -0,0 +1,96 @@
+<script>
+	import { faBold } from "@fortawesome/free-solid-svg-icons";
+
+    // pos is cursor position when right click occur
+    let pos = { x: 0, y: 0 }
+    // menu is dimension (height and width) of context menu
+    let menu = { h: 0, y: 0 }
+    // browser/window dimension (height and width)
+    let browser = { h: 0, y: 0 }
+    // showMenu is state of context-menu visibility
+    let showMenu = false;
+    // to display some text
+    /**
+	 * @type {HTMLDivElement}
+	 */
+    let content;
+
+    function rightClickContextMenu(e){
+        showMenu = true
+        browser = {
+            w: window.innerWidth,
+            h: window.innerHeight
+        };
+        pos = {
+            x: e.clientX,
+            y: e.clientY
+        };
+        // If bottom part of context menu will be displayed
+        // after right-click, then change the position of the
+        // context menu. This position is controlled by `top` and `left`
+        // at inline style. 
+        // Instead of context menu is displayed from top left of cursor position
+        // when right-click occur, it will be displayed from bottom left.
+        if (browser.h -  pos.y < menu.h)
+            pos.y = pos.y - menu.h
+        if (browser.w -  pos.x < menu.w)
+            pos.x = pos.x - menu.w
+    }
+    function onPageClick(e){
+        showMenu = false;
+    }
+    /**
+	 * @param {HTMLElement} node
+	 */
+    function getContextMenuDimension(node){
+        let height = node.offsetHeight
+        let width = node.offsetWidth
+        menu = {
+            h: height,
+            w: width
+        }
+    }
+    function setBold(){
+        content.textContent = "Add and item..."
+    }
+    function setItalic(){
+        content.textContent = "Printed..."
+    }
+    function setUnderline(){
+        content.textContent = "Zooom..."
+    }
+    let menuItems = [
+        {
+            'name': 'setBold',
+            'onClick': setBold,
+            'class': 'fa-solid fa-bold'
+        },
+        {
+            'name': 'setItalic',
+            'onClick': setItalic,
+            'class': 'fa-solid fa-italic'
+        },
+        {
+            'name': 'setUnderline',
+            'onClick': setUnderline,
+            'class': 'fa-solid fa-underline'
+        }
+    ]
+
+</script>
+
+{#if showMenu}
+    <div use:getContextMenuDimension style="position: absolute; top:{pos.y}px; left:{pos.x}px; z-index: 1">
+        <p class="buttons">
+            {#each menuItems as item}
+                <button class="button" on:click={item.onClick}>
+                    <span class="icon is-small">
+                        <i class={item.class}></i>
+                    </span>
+                </button>
+            {/each}
+        </p>
+    </div>
+{/if}
+
+<svelte:window on:contextmenu|preventDefault={rightClickContextMenu}  on:click={onPageClick} />
--- src/lib/components/Slider/TextComponent.svelte
+++ src/lib/components/Slider/TextComponent.svelte
@@ -3,45 +3,45 @@
 	import { setElementProps } from '$lib/stores/selected-element.store.ts';
 	import { textTypes } from '$lib/utils.ts';
 	import Select from '../Select/Select.svelte';
+	import ContextMenu from './ContextMenu.svelte';
 
 	export let props: Editor.TextElementProps;
 	let type: string;
 
 	function changeText(value: string): void {
 		props.text = value;
-		setElementProps({...props});
+		setElementProps({ ...props });
 	}
 
 	function changeColor(value: string): void {
 		props.appearance.color = value;
-		setElementProps({...props});
+		setElementProps({ ...props });
 	}
 
 	function changeAppearance(): void {
 		props.appearance.type = type;
-		setElementProps({...props});
+		setElementProps({ ...props });
 	}
 
+	function showContextMenu(): void {
+		const target = document.body;
+		new ContextMenu({ target });
+	}
 </script>
 
 <div class="text-container">
 	<div class="flex-column mt-1">
 		<label class="label" for="type">{@html $t('slider.text.appearance')}</label>
 		<div class="control">
-			<Select
-				id="type"
-				bind:selected={type}
-				items={textTypes} 
-				on:typeChanged={changeAppearance}
-			/>
+			<Select id="type" bind:selected={type} items={textTypes} on:typeChanged={changeAppearance} />
 		</div>
 	</div>
 	<div class="mt-1">
 		{#if props.appearance.type === 'header'}
 			<div class="control">
-				<input 
-					type="text" 
-					bind:value={props.text} 
+				<input
+					type="text"
+					bind:value={props.text}
 					class="input"
 					on:input={(ev) => changeText(ev.target.value)}
 				/>
@@ -49,8 +49,10 @@
 		{:else}
 			<div class="control">
 				<textarea
-					class="input"
+					id="textarea"
+					class="textarea"
 					on:input={(ev) => changeText(ev.target.value)}
+					on:contextmenu={() => showContextMenu()}
 				>
 					{props.text}
 				</textarea>
@@ -60,11 +62,11 @@
 	<div class="flex-column mt-1">
 		<label class="label" for="colorpicker">{@html $t('slider.text.color')}</label>
 		<div class="control">
-			<input 
-				type="color" 
+			<input
+				type="color"
 				id="colorpicker"
 				class="input"
-				bind:value={props.appearance.color} 
+				bind:value={props.appearance.color}
 				on:input={(ev) => changeColor(ev.target.value)}
 			/>
 		</div>
