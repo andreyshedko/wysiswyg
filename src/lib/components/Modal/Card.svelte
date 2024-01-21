419d9a2233beb870a7a951f7b2fbed9060599c73
andrey.shedko@gmail.com <andrey.shedko@gmail.com>
2024-01-21 15:27:51 +0100

    Add template card

---
+++ src/lib/components/Modal/Card.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let content: string;
+	export let placeholderUrl: string;
+    export let templateUrl: string;
+</script>
+
+<div class="card">
+	<div class="card-image">
+		<figure class="image is-4by3">
+			<enhanced:img src={placeholderUrl} />
+		</figure>
+	</div>
+	<div class="card-content">
+		<div class="content is-size-5-tablet is-size-5-mobile">{content}</div>
+	</div>
+    <footer class="card-footer">
+        <a data-sveltekit-preload-data="tap" href="/{templateUrl}?size=4k" class="card-footer-item is-size-6-tablet is-size-6-mobile">4K (2560px)</a>
+        <a data-sveltekit-preload-data="tap" href="/{templateUrl}?size=big_laptop" class="card-footer-item is-size-6-tablet is-size-6-mobile">Big Laptop (1440px)</a>
+        <a data-sveltekit-preload-data="tap" href="/{templateUrl}?size=laptop" class="card-footer-item is-size-6-tablet is-size-6-mobile">Laptop (1024px)</a>
+        <a data-sveltekit-preload-data="tap" href="/{templateUrl}?size=tablet" class="card-footer-item is-size-6-tablet is-size-6-mobile">Tablet (768px)</a>
+        <a data-sveltekit-preload-data="tap" href="/{templateUrl}?size=mobile" class="card-footer-item is-size-6-tablet is-size-6-mobile">Mobile (425px)</a>
+      </footer>
+</div>
--- src/lib/components/Modal/LayoutModal.svelte
+++ src/lib/components/Modal/LayoutModal.svelte
@@ -1,28 +1,27 @@
 <script lang="ts">
 	import { modal, toggleModal } from '$lib/stores/modal.store.ts';
 	import { onDestroy } from 'svelte';
+
+	import Card from '../Modal/Card.svelte';
 	import './Modal.scss';
 
 	let state: Editor.ModalState;
 
-	const unsubscribeModal = modal.subscribe((value) => state = value);
+	const unsubscribeModal = modal.subscribe((value) => (state = value));
 
 	onDestroy(() => {
 		unsubscribeModal();
 	});
 </script>
 
-<div 
-	class="modal"
-	class:is-active={state['isOpen']}
->
-	<div class="modal-background"></div>
+<div class="modal" class:is-active={state['isOpen']}>
+	<div class="modal-background" />
 	<div class="modal-content">
+		<Card
+			templateUrl={'event_reservation'}
+			content={'This is event reservation landing page template'}
+			placeholderUrl={'https://bulma.io/images/placeholders/1280x960.png'}
+		/>
 	</div>
-	<button 
-		class="modal-close is-large" 
-		aria-label="close"
-		on:click={() => toggleModal(false)}
-	>
-	</button>
-  </div>
+	<button class="modal-close is-large" aria-label="close" on:click={() => toggleModal(false)} />
+</div>
